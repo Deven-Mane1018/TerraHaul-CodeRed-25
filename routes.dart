b79f5e3aee2ce6e37d08bb9f35920c84710ea436
Deven Mane <deven.mane@adypu.edu.in>
2025-01-10 11:40:50 +0530

    Update routes.dart

--- routes.dart
+++ routes.dart
@@ -1,8 +1,1 @@
-import 'package:deep_waste/screens/SplashScreen.dart';
-import 'package:flutter/widgets.dart';
 
-// We are using name route
-// All our routes will be available here
-final Map<String, WidgetBuilder> routes = {
-  SplashScreen.routeName: (context) => SplashScreen(),
-};
